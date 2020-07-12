5d658fcf239e78097f31ec909709cb7399d70fc0
Braxton <onereanimatorwest@gmail.com>
2020-07-11 23:51:55 -0500

    So Far

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { BioComponent } from './bio/bio.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: "", redirectTo: "home", pathMatch: "full" },
+  {path: "home", component: HomeComponent},
+  {path: "contact", component: ContactComponent},
+  {path: "bio", component: BioComponent},
+  {path: "services", component: ServicesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BioComponent } from './bio/bio.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BioComponent,
+    ServicesComponent,
+    ContactComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
